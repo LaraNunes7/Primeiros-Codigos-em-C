ca66a4d3602d53c52d63989b0292907d9a3840e4
LaraNunes7 <0001065593@senaimgaluno.com.br>
2024-05-15 10:53:55 -0300

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    float media1, media2, media3, media4;
+
+    // Solicita ao usuário que insira as quatro notas
+    printf("Digite a primeira media: ");
+    scanf("%f", &media1);
+
+    printf("Digite a segunda media: ");
+    scanf("%f", &media2);
+
+	printf("Digite a terceira media: ");
+	scanf("%f", &media3);
+	
+	printf("Digite a quarta media: ");
+	scanf("%f", &media4);
+	
+
+    // Calcula a pontuação total
+    float pontuacaoMedia = media1 + media2 + media3 + media4;
+
+    // Exibe a pontuação total
+    printf("A media do aluno: %f",pontuacaoMedia); 
+
+    return 0;
+}
+
+  
