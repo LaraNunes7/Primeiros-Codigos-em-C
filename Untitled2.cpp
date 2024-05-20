5e5fb53c2e04f2f9fa3daa257088c240498cc036
LaraNunes7 <0001065593@senaimgaluno.com.br>
2024-05-20 08:33:06 -0300

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int valor1, valor2;
+
+   		printf("Digite o primeiro valor: ");
+     	scanf("%f",valor1);
+
+    	printf("Digite o segundo valor: ");
+        scanf("%f",valor2);
+
+    int diferenca = valor1 - valor2;
+
+     printf("A diferenca entre " valor1 " e " valor2 " e: diferenca ");
+
+    return 0;
+}
+
